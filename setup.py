e9fb7a66b207daff35924fe84cde6be2b96c3165
Rory Schadler <Rory.H.Schadler.21@dartmouth.edu>
2020-08-14 14:54:27 -0400

    added license, expanded setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name="test_whyis_classifier",
+    name="whyis_classifier",
     version="0.0.1",
-    packages=['whyis_classifier']
+    author="Rory Schadler",
+    author_email="rory.h.schadler.21@dartmouth.edu",
+    description="Classifier interface for Whyis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roryschadler/whyis_classifier",
+    packages=['whyis_classifier'],
+    install_requires=['rdflib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5.2'
 )
