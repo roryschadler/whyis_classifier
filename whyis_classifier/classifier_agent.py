480165bf7f940df7efaaec280f69f5e5794ed563
Rory Schadler <Rory.H.Schadler.21@dartmouth.edu>
2020-08-26 17:35:51 -0400

    overrode class methods to provide finer resolution provenance

--- whyis_classifier/classifier_agent.py
+++ whyis_classifier/classifier_agent.py
@@ -8,7 +8,8 @@ from past.utils import old_div
 import nltk, re, pprint
 from rdflib import *
 from rdflib.resource import Resource
-from time import time
+import logging
+
 
 from whyis import autonomic
 from whyis import nanopub
@@ -33,12 +34,45 @@ class Classifier(autonomic.GlobalChangeService):
 }'''
         return query
 
-    def process(self, i, o):
-        labels = []
+    # mostly copied from superclass whyis.autonomic.Service
+    def process_graph(self, inputGraph):
+        # repeat processing for every user classifier
         for name, classifier in user_classifiers:
-            labels.extend(classifier.label(i))
+            instances = self.getInstances(inputGraph)
+            results = []
+            for i in instances:
+                print("Processing", i.identifier, self)
+                output_nanopub = self.app.nanopub_manager.new()
+                o = output_nanopub.assertion.resource(i.identifier)  # OutputClass(i.identifier)
+                error = False
+                try:
+                    label, confidence = classifier.label(i)
+                    if not isinstance(label, Node):
+                        label = URIRef(label)
+                    o.add(RDF.type, label)
+                    output_nanopub.provenance.add(
+                        (output_nanopub.assertion.identifier, sio.ProbabilityMeasure, Literal(confidence)))
+                except Exception as e:
+                    output_nanopub.add(
+                        (output_nanopub.assertion.identifier, self.app.NS.sioc.content, Literal(str(e))))
+                    logging.exception("Error processing resource %s in nanopub %s" % (i.identifier, inputGraph.identifier))
+                    error = True
+                for new_np in self.app.nanopub_manager.prepare(ConjunctiveGraph(store=output_nanopub.store)):
+                    if len(new_np.assertion) == 0 and not error:
+                        continue
+                    self.explain(new_np, i, o, classifier.identifier)
+                    new_np.add((new_np.identifier, sio.isAbout, i.identifier))
+                    # print new_np.serialize(format="trig")
+                    if not self.dry_run:
+                        self.app.nanopub_manager.publish(new_np)
+                    else:
+                        print("Not publishing",new_np.identifier,", dry run.")
+                    results.append(new_np)
+        return results
 
-        for new_type in labels:
-            if not isinstance(new_type, Node):
-                new_type = URIRef(new_type)
-            o.add(RDF.type, new_type)
+    def explain(self, nanopub, i, o, classifier):
+        activity = nanopub.provenance.resource(BNode())
+        activity.add(RDF.type, self.activity_class)
+        activity.add(prov.wasAssociatedWith, classifier)
+        nanopub.pubinfo.add((o.identifier, RDF.type, self.getOutputClass()))
+        nanopub.provenance.add((nanopub.assertion.identifier, prov.wasGeneratedBy, activity.identifier))
